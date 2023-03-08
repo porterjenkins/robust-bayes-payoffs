b7f4b18dedd849001a29f3a22939bda726427a29
Porter Jenkins <porterjenkins@gmail.com>
2023-03-08 11:54:04 -0700

    outliers in generated data

--- sim/generator.py
+++ sim/generator.py
@@ -1,6 +1,7 @@
 from typing import List, Union
 import os
 
+import argparse
 import pandas as pd
 import yaml
 import numpy as np
@@ -25,6 +26,17 @@ class NegBinomProduct(object):
         y = np.random.negative_binomial(n, p, size)
         return y
 
+class OutlierSales(object):
+
+    def __init__(self, low: int = 5, high: int = 7):
+        self.low = low
+        self.high = high
+
+    def gen(self, size: int, x: np.ndarray):
+        mu = np.random.randint(self.low, self.high, size=size)
+        y = mu * x
+        return y
+
 class Store(object):
     def __init__(self, segment: int, name: str, n_samples: int, products: List[NegBinomProduct]):
         self.segment = segment
@@ -35,11 +47,12 @@ class Store(object):
 
 class SalesDataGenerator(object):
 
-    def __init__(self, name: str, n_seg: int, stores: List[Store], data_dir: str):
+    def __init__(self, name: str, n_seg: int, stores: List[Store], data_dir: str, eps: float):
         self.name = name
         self.data_dir = data_dir
         self.n_seg = n_seg
         self.stores = stores
+        self.eps = eps # outlier noise
         self.fpath = os.path.join(data_dir, name + ".csv")
 
     @classmethod
@@ -78,7 +91,8 @@ class SalesDataGenerator(object):
             name = cfg['global']['name'],
             n_seg=len(seg),
             stores=stores,
-            data_dir=cfg['global']['data_dir']
+            data_dir=cfg['global']['data_dir'],
+            eps=cfg['global']['eps']
         )
         return sim
 
@@ -97,7 +111,16 @@ class SalesDataGenerator(object):
             size = s.n_samples
             for p in s.products:
                 x = np.random.randint(1, 5, size=size)
-                y = p.gen(size=size, x=x)
+                if self.eps > 0.0:
+                    # generate random noise
+                    sales_gen = OutlierSales()
+                    noise = sales_gen.gen(size=size, x=x)
+                    mix_labs = np.random.binomial(p=self.eps, n=1, size=size)
+                    y_neg_binom = p.gen(size=size, x=x)
+                    y = noise * mix_labs + (1-mix_labs)*y_neg_binom
+
+                else:
+                    y = p.gen(size=size, x=x)
 
                 output['sales'] += y.tolist()
                 output['product'] += [p.name]*size
@@ -113,7 +136,10 @@ class SalesDataGenerator(object):
 
 
 if __name__ == "__main__":
-    sim = SalesDataGenerator.build_from_cfg("./sim_cfg.yaml")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--fpath", type=str, default="./sim_cfg.yaml")
+    args = parser.parse_args()
+    sim = SalesDataGenerator.build_from_cfg(args.fpath)
     sim.main()
 
     """p = NegBinomProduct('coke', 2.0, 2.0)
